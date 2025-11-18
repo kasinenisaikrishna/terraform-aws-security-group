e9d539072b9bb9de9b53289a2037cbd0166bf087
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-18 18:33:21 +0530

    terraform

--- main.tf
+++ main.tf
@@ -10,4 +10,11 @@ resource "aws_security_group" "main" {
         Name = local.sg_final_name
     }
   )
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
 }
