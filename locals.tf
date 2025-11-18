640156eb091cad8f973eb725c52c65e951909ab8
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-18 08:05:27 +0530

    terraform

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+  sg_final_name = "${var.project_name}-${environment}-${var.sg_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "sg_name" {
+  
+}
+
+variable "common_tags" {
+  default = {}
+}
+
+variable "sg_tags" {
+  default = {}
+}
+
+variable "vpc_id" {
+    
+}
